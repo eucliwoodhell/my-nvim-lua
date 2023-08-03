e914cb689220a26f6e0ece4a73db989a4444bc98
Paolo Castillo <paolojesus.castillo@externos-cl.cencosud.com>
2023-08-03 18:22:51 -0400

    feat(lsp): add format keyboard

--- lua/plugins/_lsp.lua
+++ lua/plugins/_lsp.lua
@@ -1,8 +1,8 @@
 local lsp = require('lsp-zero').preset({})
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
@@ -13,14 +13,18 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-    mapping = {
-        -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
-        -- Ctrl+Space to trigger completion menu
-        ['<C-Space>'] = cmp.mapping.complete(),
-
-        -- Navigate between snippet placeholder
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward()
-    }
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward()
+  }
 })
+
+-- Commands
+-- add `:Format` command to format current buffer.
+vim.api.nvim_create_user_command("Format", ":lua vim.lsp.buf.formatting()",
+  { nargs = 0 })
