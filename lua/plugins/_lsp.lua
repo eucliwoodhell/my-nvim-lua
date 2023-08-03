07c98be17f54cc7b779f2df3e877e19053aa5d08
Paolo Castillo <paolojesus.castillo@externos-cl.cencosud.com>
2023-08-03 19:56:16 -0400

    feat(lsp): add key diagnostic

--- lua/plugins/_lsp.lua
+++ lua/plugins/_lsp.lua
@@ -1,9 +1,8 @@
 local lsp = require('lsp-zero').preset({})
-local opts = { noremap = true, silent = true }
+local opts = {noremap = true, silent = true}
+local keymap = vim.api.nvim_set_keymap
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({ buffer = bufnr })
-end)
+lsp.on_attach(function(client, bufnr) lsp.default_keymaps({buffer = bufnr}) end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 lsp.setup()
@@ -13,18 +12,27 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward()
-  }
+    mapping = {
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward()
+    }
 })
 
 -- Commands
 -- add `:Format` command to format current buffer.
 vim.api.nvim_create_user_command("Format", ":lua vim.lsp.buf.formatting()",
-  { nargs = 0 })
+                                 {nargs = 0})
+
+vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+-- The following command requires plug-ins "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", and optionally "kyazdani42/nvim-web-devicons" for icon support
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+-- If you don't want to use the telescope plug-in but still want to see all the errors/warnings, comment out the telescope line and uncomment this:
+-- vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
+
