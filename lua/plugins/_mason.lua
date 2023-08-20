0d18a5013e94de2d342421398f362bb5bf3aecf8
Paolo Castillo <paolojesus.castillo@externos-cl.cencosud.com>
2023-08-20 13:53:44 -0400

    feat(eslint): add option to eslint lsp

--- lua/plugins/_mason.lua
+++ lua/plugins/_mason.lua
@@ -1,26 +1,46 @@
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
     }
+  }
 })
 
 require("mason-lspconfig").setup({
-    automatic_installation = true,
-    ensure_installed = {
-      "jsonls",
-      "cssls",
-      "tailwindcss",
-      "pylsp",
-      "jedi_language_server",
-      "rust_analyzer",
-      "tsserver",
-      "eslint",
-      "dockerls",
-      "bashls",
-      "sqlls"
-    },
+  automatic_installation = true,
+  ensure_installed = {
+    "jsonls",
+    "cssls",
+    "tailwindcss",
+    "pylsp",
+    "jedi_language_server",
+    "rust_analyzer",
+    "tsserver",
+    "eslint",
+    "dockerls",
+    "bashls",
+    "sqlls"
+  },
+})
+
+require("lspconfig").eslint.setup({
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "rust"
+  },
+  tsserver = {
+    format = { enable = false }
+  },
+  eslint = {
+    enable = true,
+    format = { enable = true },
+    codeActionsOnSave = {
+      mode = "all",
+    }
+  }
 })
