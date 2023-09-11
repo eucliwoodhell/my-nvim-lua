3d8fd43f0e9814047c4dd5625d559fc989c112c4
Paolo Castillo <paolojesus.castillo@externos-cl.cencosud.com>
2023-09-11 12:00:04 -0300

    fix(key): add config ket and lspsaga

--- lua/config/_keymap.lua
+++ lua/config/_keymap.lua
@@ -1,4 +1,4 @@
--- vim.opt = read and write general option 
+-- vim.opt = read and write general option
 -- vim.o   = read and write option editor
 -- vim.wo  = read and write value windows
 -- vim.bo  = read and write file buffer
@@ -11,47 +11,47 @@ local editor_option = vim.o
 -- local keymap         = vim.api.nvim_set_keymap
 local keymap = vim.keymap.set
 
-local opt = {noremap = true, silent = true, expr = true}
+local opt = { noremap = true, silent = true, expr = true }
 
 editor_option.shortmess = editor_option.shortmess .. 'c'
 
 local set_opts = function(opts)
-    local noremap = opts.noremap == nil and true or opts.noremap
-    local silent = opts.silent == nil and true or opts.silent
-    local desc = opts.desc or nil
-    return {noremap = noremap, silent = silent, desc = desc}
+  local noremap = opts.noremap == nil and true or opts.noremap
+  local silent = opts.silent == nil and true or opts.silent
+  local desc = opts.desc or nil
+  return { noremap = noremap, silent = silent, desc = desc }
 end
 
 keymap('', '<Space>', '<Nop>', opt)
 
 -- Actions keys
 keymap("n", "<leader>w", function() vim.cmd("write") end,
-       set_opts({desc = "Saves buffer"}))
+  set_opts({ desc = "Saves buffer" }))
 keymap("n", "<leader>W", function() vim.cmd("w!") end,
-       set_opts({desc = "Saves buffer!"}))
+  set_opts({ desc = "Saves buffer!" }))
 keymap("n", "<leader>q", function() vim.cmd("quit") end,
-       set_opts({desc = "Quit buffer"}))
+  set_opts({ desc = "Quit buffer" }))
 keymap("n", "<leader>wq", function() vim.cmd("DeleteCurrentBuffer") end,
-       set_opts({desc = "Save and quit Buffer"}))
+  set_opts({ desc = "Save and quit Buffer" }))
 keymap("n", "<leader>F", function() vim.cmd("Format") end,
-       set_opts({desc = "Format current buffer"}))
+  set_opts({ desc = "Format current buffer" }))
 keymap("n", "<leader>m", function() vim.cmd("bnext") end,
-       set_opts({desc = "Next buffer"}))
+  set_opts({ desc = "Next buffer" }))
 keymap("n", "<leader>z", function() vim.cmd("bprevious") end,
-       set_opts({desc = "Previous buffer"}))
+  set_opts({ desc = "Previous buffer" }))
 keymap("n", "<leader>q", function() vim.cmd("bdelete") end,
-       set_opts({desc = "Close buffer"}))
+  set_opts({ desc = "Close buffer" }))
 keymap("n", "<leader>nm", function() vim.cmd("messages") end,
-       set_opts({desc = "Messages"}))
+  set_opts({ desc = "Messages" }))
 keymap("n", "<leader>Q", function() vim.cmd("wq!") end,
-       set_opts({desc = "Quit"}))
+  set_opts({ desc = "Quit" }))
 keymap("n", "<leader>pf",
-       "<CMD>lua require('telescope.builtin').grep_string({ initial_mode = 'normal' })<CR>",
-       set_opts({desc = "Search"}))
+  "<CMD>lua require('telescope.builtin').grep_string({ initial_mode = 'normal' })<CR>",
+  set_opts({ desc = "Search" }))
 
 -- move keys
-keymap("", "Z", "^", set_opts({desc = "cursor right"}))
-keymap("", "X", "$", set_opts({desc = "cursor left"}))
+keymap("", "Z", "^", set_opts({ desc = "cursor right" }))
+keymap("", "X", "$", set_opts({ desc = "cursor left" }))
 
 -- move line up and down
 keymap("", "Ω", "<Esc>:m .-2<CR>===", {})
@@ -59,39 +59,39 @@ keymap("", "µ", "<Esc>:m .+1<CR>===", {})
 
 -- windows managing
 keymap("n", "<leader>WS", function() vim.cmd("split") end,
-       set_opts({desc = "HSplit"}))
+  set_opts({ desc = "HSplit" }))
 keymap("n", "<leader>WV", function() vim.cmd("vsplit") end,
-       set_opts({desc = "VSplit"}))
+  set_opts({ desc = "VSplit" }))
 
 -- resize windows
 keymap("n", "<CR-S-Up>", function() vim.cmd("resize +2") end,
-       set_opts({desc = "Increase windows height "}))
+  set_opts({ desc = "Increase windows height " }))
 keymap("n", "<CR-S-Down>", function() vim.cmd("resize -2") end,
-       set_opts({desc = "Decrease windows height"}))
+  set_opts({ desc = "Decrease windows height" }))
 keymap("n", "<CR-S-Left>", function() vim.cmd("vertical resize -2") end,
-       set_opts({desc = "Decrease windows width"}))
+  set_opts({ desc = "Decrease windows width" }))
 keymap("n", "<CR-S-Right>", function() vim.cmd("vertical resize +2") end,
-       set_opts({desc = "Increase windows width"}))
+  set_opts({ desc = "Increase windows width" }))
 
 -- delete & cut
 keymap("n", "x", [["_x]], set_opts({}))
-keymap({"n", "v"}, "d", [["_d]], set_opts({}))
+keymap({ "n", "v" }, "d", [["_d]], set_opts({}))
 keymap("n", "F", [["_D]], set_opts({}))
 keymap("n", "D", [["_d0]], set_opts({}))
 
 -- search and replace
 -- :cd to the directory of the current file
 keymap("n", "<leader>r", [[:%s/\V<C-r>///g<left><left>]],
-       {desc = "Search text and replace"})
+  { desc = "Search text and replace" })
 keymap("v", "<leader>r", [[:'<,'>s/\V<C-r>///g<left><left>]],
-       {desc = "Search text and replace"})
+  { desc = "Search text and replace" })
 keymap("n", "<leader>g",
-       [[:vimgrep /\V<C-r>//gj **/*<c-r>=setcmdpos(getcmdpos()-8)[1]<cr>]],
-       {desc = "Search text and replace all files"})
+  [[:vimgrep /\V<C-r>//gj **/*<c-r>=setcmdpos(getcmdpos()-8)[1]<cr>]],
+  { desc = "Search text and replace all files" })
 -- :copen to open the quickfix window
 keymap("n", "<leader>gr",
-       [[:cfdo %s/\V<C-r>///gc | update<c-r>=setcmdpos(getcmdpos()-12)[1]<cr>]],
-       {desc = "Open quickfix window and replace"})
+  [[:cfdo %s/\V<C-r>///gc | update<c-r>=setcmdpos(getcmdpos()-12)[1]<cr>]],
+  { desc = "Open quickfix window and replace" })
 
 keymap('n', '<leader>t', ':WhichKey<CR>', {})
 keymap('n', '<leader>tf', ':WhichKey <leader><CR>', {})
@@ -100,3 +100,4 @@ keymap('n', '<leader>tf', ':WhichKey <leader><CR>', {})
 --  <c-r>=setcmdpos(getcmdpos()-31)[1]<cr>
 --  <left><left>
 --  repeat('<left>', 31)
+-- ctrl+v select multiple line
--- lua/plugins/_lspsaga.lua
+++ lua/plugins/_lspsaga.lua
@@ -1,43 +1,63 @@
 local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 
 require('lspsaga').setup {
-    debug = false,
-    use_saga_diagnostic_sign = true,
-    -- diagnostic sign
-    error_sign = "",
-    warn_sign = "",
-    hint_sign = "",
-    infor_sign = "",
-    diagnostic_header_icon = "   ",
-    -- code action title icon
-    code_action_icon = " ",
-    code_action_prompt = {
-        enable = true,
-        sign = true,
-        sign_priority = 40,
-        virtual_text = true
-    },
-    finder_definition_icon = "  ",
-    finder_reference_icon = "  ",
-    max_preview_lines = 10,
-    finder_action_keys = {
-        open = "o",
-        vsplit = "s",
-        split = "i",
-        quit = "q",
-        scroll_down = "<C-f>",
-        scroll_up = "<C-b>"
-    },
-    code_action_keys = {quit = "q", exec = "<CR>"},
-    rename_action_keys = {quit = "<C-c>", exec = "<CR>"},
-    definition_preview_icon = "  ",
-    border_style = "single",
-    rename_prompt_prefix = "➤",
-    server_filetype_map = {},
-    diagnostic_prefix_format = "%d. "
+  debug = false,
+  use_saga_diagnostic_sign = true,
+  -- diagnostic sign
+  error_sign = "",
+  warn_sign = "",
+  hint_sign = "",
+  infor_sign = "",
+  diagnostic_header_icon = "   ",
+  -- code action title icon
+  code_action_icon = " ",
+  code_action_prompt = {
+    enable = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true
+  },
+  finder_definition_icon = "  ",
+  finder_reference_icon = "  ",
+  max_preview_lines = 10,
+  finder_action_keys = {
+    open = "o",
+    vsplit = "s",
+    split = "i",
+    quit = "q",
+    scroll_down = "<C-f>",
+    scroll_up = "<C-b>"
+  },
+  code_action_keys = { quit = "q", exec = "<CR>" },
+  rename_action_keys = { quit = "<C-c>", exec = "<CR>" },
+  definition_preview_icon = "  ",
+  border_style = "single",
+  rename_prompt_prefix = "➤",
+  server_filetype_map = {},
+  diagnostic_prefix_format = "%d. "
 }
 
-keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", {silent = true})
-keymap("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", {silent = true})
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+keymap("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
 keymap("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>",
-       {silent = true})
+  { silent = true })
+
+
+keymap("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { silent = true })
+
+if vim.fn.exists(":Telescope") then
+  keymap('n', '<leader>gr', '<cmd>Telescope lsp_references<CR>', opts)
+  keymap('n', '<leader>gd', '<cmd>Telescope lsp_definitions<CR>', opts)
+  keymap('n', '<leader>gi', '<cmd>Telescope lsp_implementations<CR>', opts)
+  keymap('n', '<leader>gt', '<cmd>Telescope lsp_type_definitions<CR>', opts)
+else
+  keymap("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  keymap("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  keymap("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  keymap("n", "<leader>gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+end
+
+keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
